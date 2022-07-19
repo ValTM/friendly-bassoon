e14e31b4d8a311103189ae730d1f5644888fb083
Mariyan <mariqn.nz05@gmail.com>
2022-07-19 18:39:03 +0300

    setup project fixed

--- friendly-bassoon/src/components/ProjectsBtn.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function ProjectsBtn() {
-  return <div>projectsBtn</div>;
-}
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    spacing: {
+      1: '8px',
+      2: '12px',
+      3: '16px',
+      4: '24px',
+      5: '32px',
+      6: '50px',
+      7: '75px',
+      8: '100px',
+      9: '200px',
+      10: '300px',
+    },
+  },
+  plugins: [],
+};
