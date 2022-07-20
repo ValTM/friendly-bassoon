556cb63046febefbc224519a9fb37e95a55cc543
Mariyan <mariqn.nz05@gmail.com>
2022-07-20 21:18:18 +0300

    setup finally fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Effdit{' '}
+          <code className="text-3xl font-bold underline">src/App.jsx</code> and
+          save to test HMR
+        </p>
+      </div>
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
 }
 
 export default App;
