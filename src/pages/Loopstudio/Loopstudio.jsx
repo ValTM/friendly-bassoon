7d2c67f443b4a9b20f49219753bdf3090af1d69d
Mariyan <mariqn.nz05@gmail.com>
2022-07-23 15:07:43 +0300

    setup Landing Page

---
+++ src/pages/Loopstudio/Loopstudio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function Loopstudio() {
+  return (
+    <div>
+      <Header title="Loopstudio" />
+    </div>
+  );
+}
+
+export default Loopstudio;
