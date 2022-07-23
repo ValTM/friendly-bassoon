738263be2ca0b97041f2fbc8150020460628f630
Mariyan <mariqn.nz05@gmail.com>
2022-07-23 15:07:43 +0300

    setup Landing Page

---
+++ src/pages/GithubApp/GithubApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function GithubApp() {
+  return (
+    <div>
+      <Header title="Github App" />
+    </div>
+  );
+}
+
+export default GithubApp;
