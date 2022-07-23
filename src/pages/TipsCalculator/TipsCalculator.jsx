9d53bcfa7bc4a972ae20c64e421d7bdbb0351119
Mariyan <mariqn.nz05@gmail.com>
2022-07-23 15:07:43 +0300

    setup Landing Page

---
+++ src/pages/TipsCalculator/TipsCalculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function TipsCalculator() {
+  return (
+    <div>
+      <Header title="Tips Calculator" />
+    </div>
+  );
+}
+
+export default TipsCalculator;
