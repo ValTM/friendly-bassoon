578be2e4be4beea286cf25661e29335d4db053ad
Mariyan <mariqn.nz05@gmail.com>
2022-07-23 15:53:14 +0300

    added card boxes in Landing page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../pages/LandingPage/LandingPage.css';
 
-function Card() {
-  return <div>Card</div>;
+function Card(props) {
+  return (
+    <div className="cardBox m-3">
+      <h1>{props.title}</h1>
+      <img src={props.image} alt="" />
+    </div>
+  );
 }
+
+Card.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.element,
+};
 
 export default Card;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './LandingPage.css';
+import Card from '../../components/Card';
 
 function LandingPage() {
   return (
     <>
-      <div className=" homeContainer container bg-white shadow-2xl mx-auto  h-44 flex  flex-col justify-center text-center sm:h-64 sm:mt-20 lg:mt-32  ">
-        <h1 className="inline-block mt-2 text-2xl font-sans sm:text-4xl ">
+      <div className=" homeContainer container bg-white shadow-2xl mx-auto  flex  flex-col justify-center text-center  sm:mt-20 lg:mt-32  ">
+        <h1 className="inline-block mt-20 text-2xl font-sans sm:text-4xl ">
           Welcome to the Home Page
         </h1>
         <p className="mt-3 text-1xl font-sans sm:text-2xl">
           Here you can see{' '}
           <span className="text-red-600">all the projects</span>
         </p>
+        <div className=" flex flex-col mx-auto  mt-10 mb-20 lg:flex-row lg:flex-wrap lg:justify-center">
+          {' '}
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </>
   );
