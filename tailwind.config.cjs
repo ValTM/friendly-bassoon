01eb87f5563fd5f5d0d55222f59823b986af32d3
Mariyan <mariqn.nz05@gmail.com>
2022-07-24 13:29:51 +0300

    added custom gray colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,12 @@ module.exports = {
     colors: {
       white: '#fff',
       black: '#000',
+      gray: {
+        100: '#f3f4f6',
+        200: '#e5e7eb',
+        300: '#d1d5db',
+      },
+      // Colors for Loopstudio project
       loopDarkGray: '#696969',
     },
   },
