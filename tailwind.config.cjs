f2abb1fb86c7bec99218fdb9ca0bbf2de08d1f33
Mariyan <mariqn.nz05@gmail.com>
2022-07-24 16:14:04 +0300

    added Hero Section in Equalizer project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,8 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
-    fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
-    },
     colors: {
+      // Main custom colors
       white: '#fff',
       black: '#000',
       gray: {
@@ -13,8 +11,16 @@ module.exports = {
         200: '#e5e7eb',
         300: '#d1d5db',
       },
+
       // Colors for Loopstudio project
       loopDarkGray: '#696969',
+
+      // Colors for Equalizer project
+      eqGreen: '#66E2DC',
+      eqOrange: '#FA7453',
+      eqYellow: '#FFB964',
+      eqBlack: '#191826',
+      eqWhite: '#FCFAF9',
     },
   },
   plugins: [],
