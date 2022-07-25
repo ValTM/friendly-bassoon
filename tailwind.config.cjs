4442767f528802d57fae6f50ba44ec17706bf48a
Mariyan <mariqn.nz05@gmail.com>
2022-07-25 15:25:42 +0300

    added Tips Calculator body

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,8 +13,11 @@ module.exports = {
         200: '#e5e7eb',
         300: '#d1d5db',
       },
-      // Colors for Loopstudio project
-      loopDarkGray: '#696969',
+      //Colors for Tips Calculator project
+      tipsCyan: '#26c0ab',
+      tipsDarkCyan: '#00494d',
+      tipsGrayishCyan: '#5e7a7d',
+      tipsLightGrayishCyan: '#c5e4e7',
     },
   },
   plugins: [],
