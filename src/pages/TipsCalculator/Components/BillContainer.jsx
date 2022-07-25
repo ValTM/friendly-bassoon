da6a47c163f4c6c611267e452d5e4ed522ea6e1b
Mariyan <mariqn.nz05@gmail.com>
2022-07-25 17:46:55 +0300

    added invalid chars file in Tips Calculator project

--- src/pages/TipsCalculator/Components/BillContainer.jsx
+++ src/pages/TipsCalculator/Components/BillContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import dollar from '../../../assets/tips-calc-img/icon-dollar.svg';
+import { blockInvalidChar } from '../invalidChar';
 
 function BillContainer() {
   return (
@@ -11,6 +12,7 @@ function BillContainer() {
         className=" mt-2 bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold focus:outline-none focus:border-tipsCyan focus:ring-tipsCyan focus:ring-2"
         type="number"
         placeholder="0"
+        onKeyDown={blockInvalidChar}
       />
       <img
         className="absolute bottom-3 left-4 right-2 w-[15px]"
