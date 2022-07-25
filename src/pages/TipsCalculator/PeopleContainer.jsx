f1c76a2bcc7e3845eaa0b402a8158a8b2b078e61
Mariyan <mariqn.nz05@gmail.com>
2022-07-25 17:00:00 +0300

    added focus on imput elements in Tips Calculator project

--- src/pages/TipsCalculator/PeopleContainer.jsx
+++ src/pages/TipsCalculator/PeopleContainer.jsx
@@ -8,7 +8,7 @@ function PeopleContainer() {
         Number of People
       </label>
       <input
-        className=" mt-2 bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold"
+        className=" mt-2 bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold focus:outline-none focus:border-tipsCyan focus:ring-tipsCyan focus:ring-2"
         type="number"
         placeholder="0"
       />
