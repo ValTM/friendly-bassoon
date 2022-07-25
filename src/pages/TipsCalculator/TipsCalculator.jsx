799b083b12e45b714aa1cce72f74c10f33b2e155
Mariyan <mariqn.nz05@gmail.com>
2022-07-25 17:40:27 +0300

    made some changes in Tips Calculator project

--- src/pages/TipsCalculator/TipsCalculator.jsx
+++ src/pages/TipsCalculator/TipsCalculator.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Header from '../../components/Header';
 import './TipsCalculator.css';
-import BillContainer from './BillContainer';
+import BillContainer from './Components/BillContainer';
 import logo from '../../assets/tips-calc-img/logo.svg';
-import TipsContainer from './TipsContainer';
-import PeopleContainer from './PeopleContainer';
-import RecapContainer from './RecapContainer';
+import TipsContainer from '././Components/TipsContainer';
+import PeopleContainer from '././Components/PeopleContainer';
+import RecapContainer from '././Components/RecapContainer';
 
 function TipsCalculator() {
   return (
     <div className="tips-body h-screen bg-tipsLightGrayishCyan">
       <Header title="Tips Calculator" />
       <img className="mx-auto pt-10" src={logo} alt="" />
-      <form className=" bg-white mt-[100px] rounded-t-[20px] p-10 xl:w-[80%] mx-auto xl:rounded-xl 2xl:w-[65%]   ">
+      <form className=" bg-white mt-[100px] rounded-t-[20px] p-10 xl:w-[80%] mx-auto xl:rounded-xl 2xl:w-[1300px]   ">
         <div className="xl:flex xl:justify-between">
           <div className="xl:w-[48%]">
             <BillContainer />
