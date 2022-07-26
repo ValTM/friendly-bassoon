8e5adc8dc3b5040ff7c51b7809fcb7e1633be339
Mariyan <mariqn.nz05@gmail.com>
2022-07-27 02:17:11 +0300

    optimised Tips Calculator project

--- src/pages/TipsCalculator/Components/TipsContainer.jsx
+++ src/pages/TipsCalculator/Components/TipsContainer.jsx
@@ -6,6 +6,20 @@ function TipsContainer({ setTip }) {
   TipsContainer.propTypes = {
     setTip: PropTypes.node.is,
   };
+  const handleButtonClick = (e) => {
+    setTip(parseInt(e.target.value));
+    const btn = document.getElementById(e.target.id);
+    let btns = Array.from(document.getElementById('btn--container').children);
+    console.log(btns);
+    for (let btn of btns) {
+      if (btn.classList.contains('bg-tipsCyan')) {
+        btn.classList.remove('bg-tipsCyan');
+        btn.classList.add('bg-tipsDarkCyan');
+      }
+    }
+    btn.classList.add('bg-tipsCyan');
+  };
+
   const maxLengthCheck = (event) => {
     if (event.target.value.length > event.target.maxLength) {
       event.target.value = event.target.value.slice(1, event.target.maxLength);
@@ -19,39 +33,44 @@ function TipsContainer({ setTip }) {
       <label className="text-tipsDarkCyan font-bold" htmlFor="tip">
         Select Tip %
       </label>
-      <div className="flex flex-wrap justify-between mt-2">
+      <div id="btn--container" className=" flex flex-wrap justify-between mt-2">
         <button
+          id="1"
           className=" my-2  w-[49%] py-2 bg-tipsDarkCyan text-tipsWhiteColor text-center text-2xl rounded-lg font-bold hover:bg-tipsCyan hover:text-tipsDarkCyan "
           value={5}
-          onClick={(e) => setTip(parseInt(e.target.value))}
+          onClick={handleButtonClick}
         >
           5%
         </button>
         <button
+          id="2"
           className="my-2  w-[49%] py-2 bg-tipsDarkCyan text-tipsWhiteColor text-center text-2xl rounded-lg font-bold hover:bg-tipsCyan hover:text-tipsDarkCyan"
           value={10}
-          onClick={(e) => setTip(parseInt(e.target.value))}
+          onClick={handleButtonClick}
         >
           10%
         </button>
         <button
+          id="3"
           className="my-2  w-[49%] py-2 bg-tipsDarkCyan text-tipsWhiteColor text-center text-2xl rounded-lg font-bold hover:bg-tipsCyan hover:text-tipsDarkCyan"
           value={15}
-          onClick={(e) => setTip(parseInt(e.target.value))}
+          onClick={handleButtonClick}
         >
           15%
         </button>
         <button
+          id="4"
           className="my-2  w-[49%] py-2 bg-tipsDarkCyan text-tipsWhiteColor text-center text-2xl rounded-lg font-bold hover:bg-tipsCyan hover:text-tipsDarkCyan"
           value={25}
-          onClick={(e) => setTip(parseInt(e.target.value))}
+          onClick={handleButtonClick}
         >
           25%
         </button>
         <button
+          id="5"
           className="my-2  w-[49%] py-2 bg-tipsDarkCyan text-tipsWhiteColor text-center text-2xl rounded-lg font-bold hover:bg-tipsCyan hover:text-tipsDarkCyan"
           value={50}
-          onClick={(e) => setTip(parseInt(e.target.value))}
+          onClick={handleButtonClick}
         >
           50%
         </button>
