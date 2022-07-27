53c0bbc8d7621f0e5f292f4044301472259880fa
Mariyan <mariqn.nz05@gmail.com>
2022-07-27 23:55:51 +0300

    deleted feature/Loopstudio branch

--- src/pages/Loopstudio/MainSection/MainGallery/MainGallery.jsx
+++ src/pages/Loopstudio/MainSection/MainGallery/MainGallery.jsx
@@ -8,12 +8,12 @@ function MainGallery() {
       <h1 className="text-center gallery--text text-2xl uppercase lg:hidden ">
         Our Creations
       </h1>
-      <div className=" gallery--nav hidden lg:flex lg:justify-between lg:w-[860px] lg:mx-auto xl:w-[1170px]">
+      <div className="lg:w-[860px] lg:mx-auto hidden mx-20 lg:flex lg:justify-between 2xl:w-[1170px]">
         <h1 className="text-center gallery--text text-4xl uppercase ">
           Our Creations
         </h1>
         <a
-          className="border border-loopDarkGray border-black text-black px-8 py-1 text-2xl uppercase tracking-widest hover:bg-black hover:text-white hover:border duration-300 "
+          className="border-2 border-black px-5 py-1 text-2xl uppercase tracking-widest hover:bg-black hover:text-white hover:border-2 duration-300 "
           href=""
         >
           See All
@@ -22,7 +22,7 @@ function MainGallery() {
       <div className="items justify-center grid mt-10 gap-x-14 gap-y-8 lg:flex flex-wrap ">
         <ShowcaseCard
           cardClassName="loop-earth item "
-          textClassName="items-text absolute bottom-2 left-5 text-white uppercase text-2xl lg:text-3xl lg:bottom-4"
+          textClassName="items-text absolute bottom-2 left-5 text-white uppercase text-2xl lg:text-3xl lg:bottom-4 "
           text1="deep"
           text2="earth"
         />
