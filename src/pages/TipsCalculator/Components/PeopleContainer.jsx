33f0ee50bc51e7de2c8390dc97d33bf89d7288a9
Val TM <valeri4ew@yahoo.com>
2022-10-18 22:05:49 +0300

    People container reuse DOM

--- src/pages/TipsCalculator/Components/PeopleContainer.jsx
+++ src/pages/TipsCalculator/Components/PeopleContainer.jsx
@@ -6,50 +6,34 @@ import PropTypes from 'prop-types';
 function PeopleContainer({ nbPeople, setNbPeople }) {
   return (
     <div className="mt-10 flex flex-col relative">
-      {nbPeople === 0 ? (
-        <div>
-          {' '}
-          <label className="text-tipsDarkCyan font-bold" htmlFor="bill">
-            Number of People
-          </label>
-          <input
-            className="mt-2 w-[100%] bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold focus:outline-none focus:border-[#e11d48] focus:ring-[#e11d48] focus:ring-2"
-            type="number"
-            placeholder="0"
-            onKeyDown={blockInvalidChar}
-            required
-            onChange={(e) => setNbPeople(parseFloat(e.target.value))}
-          />
+      <div>
+        <label className="text-tipsDarkCyan font-bold" htmlFor="bill">
+          Number of People
+        </label>
+        <input
+          className={`mt-2 w-[100%] bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold focus:outline-none ${
+            nbPeople === 0
+              ? 'focus:border-[#e11d48] focus:ring-[#e11d48]'
+              : 'focus:border-tipsCyan focus:ring-tipsCyan'
+          } focus:ring-2`}
+          type="number"
+          placeholder="0"
+          min={0}
+          onKeyDown={blockInvalidChar}
+          required
+          onChange={(e) => setNbPeople(parseFloat(e.target.value))}
+        />
+        {nbPeople === 0 && (
           <p className="absolute right-2 -top-5 text-[#e11d48] font-bold text-md sm:top-0">
             Can&apos;t be zero
           </p>
-          <img
-            className="absolute bottom-3 left-4 right-2 w-[15px]"
-            src={people}
-            alt=""
-          />
-        </div>
-      ) : (
-        <div>
-          {' '}
-          <label className="text-tipsDarkCyan font-bold" htmlFor="bill">
-            Number of People
-          </label>
-          <input
-            className="mt-2 w-[100%] bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold focus:outline-none focus:border-tipsCyan focus:ring-tipsCyan focus:ring-2"
-            type="number"
-            placeholder="0"
-            onKeyDown={blockInvalidChar}
-            required
-            onChange={(e) => setNbPeople(parseFloat(e.target.value))}
-          />
-          <img
-            className="absolute bottom-3 left-4 right-2 w-[15px]"
-            src={people}
-            alt=""
-          />
-        </div>
-      )}
+        )}
+        <img
+          className="absolute bottom-3 left-4 right-2 w-[15px]"
+          src={people}
+          alt=""
+        />
+      </div>
     </div>
   );
 }
