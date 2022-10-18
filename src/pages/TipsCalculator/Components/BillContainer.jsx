42bc0f5b737b435cbfdaf09adadef821f0223c4a
Val TM <valeri4ew@yahoo.com>
2022-10-18 22:05:11 +0300

    Set bill container input minimum as 0

--- src/pages/TipsCalculator/Components/BillContainer.jsx
+++ src/pages/TipsCalculator/Components/BillContainer.jsx
@@ -13,6 +13,7 @@ function BillContainer({ setBill }) {
         className="mt-2 bg-tipsWhiteColor h-12 rounded-md text-right pr-4 pl-9 text-2xl text-tipsDarkCyan font-bold focus:outline-none focus:border-tipsCyan focus:ring-tipsCyan focus:ring-2"
         type="number"
         placeholder="0"
+        min={0}
         onKeyDown={blockInvalidChar}
         required
         onChange={(e) => setBill(parseFloat(e.target.value))}
